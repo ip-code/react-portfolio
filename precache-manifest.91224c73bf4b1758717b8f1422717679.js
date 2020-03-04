0b23687e76393b4315e092cfc84eea369d641eac
ipcode <pulivan@gmail.com>
2020-03-04 11:29:38 +0100

    Updates

---
+++ precache-manifest.91224c73bf4b1758717b8f1422717679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98e9d09cb99c441a0470f1cd3beea0d",
+    "url": "/react-typescript-portfolio/index.html"
+  },
+  {
+    "revision": "805890b974ab3174d829",
+    "url": "/react-typescript-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1aa5065f511229d01ea0",
+    "url": "/react-typescript-portfolio/static/js/2.66053dac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-typescript-portfolio/static/js/2.66053dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "805890b974ab3174d829",
+    "url": "/react-typescript-portfolio/static/js/main.fa3e43c9.chunk.js"
+  },
+  {
+    "revision": "eef07219261bf2f9c392",
+    "url": "/react-typescript-portfolio/static/js/runtime-main.9f2608f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-typescript-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
