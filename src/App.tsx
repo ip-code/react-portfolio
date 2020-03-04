7d62b405275510d349f88501861a6481b4ce5488
Ivan Pul <pulivan@gmail.com>
2020-03-04 17:28:00 +0100

    ESLint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import Sidebar from './components/sidebar';
 import './assets/styles/App.css';
+import { Fade } from 'react-awesome-reveal';
+import ImageAvatar from './assets/images/avatar.png';
 
 function App() {
   return (
+    <Fade>
     <div className="App">
       <header className="App-header">
-        <Sidebar />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <img src={ImageAvatar} alt="profile pic"/>
+          <h3>Ip Code</h3>
+          <p className="location">
+            <i className="fas fa-map-marker-alt"></i>
+            Solar system, Earth
+          </p>
+          <p className="email">
+            <i className="fas fa-envelope"></i>
+            ip-code@spacemail.com
+          </p>
+          <p>Developer Trainee</p>
+        
       </header>
     </div>
+    </Fade>
+
   );
 }
 
