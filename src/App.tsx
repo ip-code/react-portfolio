3fd455773954456c19e824692cdac156151b6a82
ipcode <pulivan@gmail.com>
2020-03-04 14:59:49 +0100

    Added sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/sidebar';
+import './assets/styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Sidebar />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
